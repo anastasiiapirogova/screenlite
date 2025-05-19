58fbf96f47ba008cc969e24e4d101b6ad7a17676
514sid <140138716+514sid@users.noreply.github.com>
2025-05-19 21:45:38 +0400

    refactor

--- server/src/modules/device/helpers/generateConnectionCode.ts
+++ server/src/modules/device/helpers/generateConnectionCode.ts
@@ -1,20 +1,21 @@
 import crypto from 'crypto'
 
-export const generateConnectionCode = (): string => {
-    // Some characters are excluded because they can be easily confused with others
-    // (e.g., 'I' with '1', 'O' with '0') to improve readability.
-    const characters = 'BCDFGHJKMPQRTVWXY346789'
+// Some characters are excluded because they can be easily confused with others
+// (e.g., 'I' with '1', 'O' with '0') to improve readability.
+const CHARACTERS = 'BCDFGHJKMPQRTVWXY346789'
+const CODE_LENGTH = 6
 
+export const generateConnectionCode = (): string => {
     let result = ''
 
-    const bytes = crypto.randomBytes(6)
+    for (let i = 0; i < CODE_LENGTH; i++) {
+        let byte: number
+        const maxValidByte = 256 - (256 % CHARACTERS.length)
 
-    for (let i = 0; i < 6; i++) {
-        let byte;
         do {
-            byte = crypto.randomBytes(1)[0];
-        } while (byte >= 256 - (256 % characters.length));
-        result += characters.charAt(byte % characters.length);
+            byte = crypto.randomBytes(1)[0]
+        } while (byte >= maxValidByte)
+        result += CHARACTERS[byte % CHARACTERS.length]
     }
 
     return result
