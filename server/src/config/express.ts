ad9a0f3ba6cbacc357b3a409e7778d9cce740db4
514sid <140138716+514sid@users.noreply.github.com>
2025-06-04 19:38:04 +0400

    fix: update server log message to include application name

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -49,7 +49,7 @@ app.use(multerErrorHandler)
 app.use(errorHandler)
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000')
+    console.log('Screenlite: Server running on port 3000')
 })
 
 export { app, server }
