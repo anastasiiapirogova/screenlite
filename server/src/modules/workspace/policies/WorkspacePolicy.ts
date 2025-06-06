642476e59d529b7212a9d63464e15ff82e4bb067
514sid <140138716+514sid@users.noreply.github.com>
2025-06-06 22:04:26 +0400

    refactor: improve canViewWorkspace method to handle workspaces without members

--- server/src/modules/workspace/policies/WorkspacePolicy.ts
+++ server/src/modules/workspace/policies/WorkspacePolicy.ts
@@ -1,14 +1,18 @@
 import { UserWorkspace } from 'generated/prisma/client.js'
 import { SafeUser } from 'types.js'
-import { getUserWorkspacePermissionsEager } from '../utils/getUserWorkspacePermissions.js'
+import { getUserWorkspacePermissions, getUserWorkspacePermissionsEager } from '../utils/getUserWorkspacePermissions.js'
 
 export class WorkspacePolicy {
     static PERMISSIONS = {
         UPDATE_WORKSPACE: 'update_workspace',
     }
 
-    static async canViewWorkspace(user: SafeUser, workspace: { members: UserWorkspace[] }): Promise<boolean> {
-        return getUserWorkspacePermissionsEager(user, workspace.members)
+    static async canViewWorkspace(user: SafeUser, workspace: { id?: string, members?: UserWorkspace[] }): Promise<boolean> {
+        if(!workspace.members) {
+            return getUserWorkspacePermissions(user, workspace.id)
+        } else {
+            return getUserWorkspacePermissionsEager(user, workspace.members)
+        }
     }
 
     static async canUpdateWorkspace(user: SafeUser, workspace: { members: UserWorkspace[] }): Promise<boolean> {
