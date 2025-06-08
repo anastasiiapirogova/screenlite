2785627b10d98442564be2e4fbe546adf8c70b3b
514sid <140138716+514sid@users.noreply.github.com>
2025-06-09 00:34:11 +0400

    feat: restructure WorkspaceFileUploadPage layout and integrate FileUploader component

--- client/src/modules/file/pages/WorkspaceFileUploadPage.tsx
+++ client/src/modules/file/pages/WorkspaceFileUploadPage.tsx
@@ -1,19 +1,30 @@
 import { useFileUploadingStorage } from '@stores/useFileUploadingStorage'
 import { UploadFilesDropzone } from '../components/UploadFilesDropzone'
 import { FileUploadingCard } from '../components/FileUploadingCard'
+import { FileUploader } from '../components/FileUploader'
+import { useUploadingPageLeaveInterceptor } from '../hooks/useUploadingPageLeaveInterceptor'
+import { LayoutBodyContainer } from '@shared/components/LayoutBodyContainer'
+import { ScrollArea } from '@shared/ui/ScrollArea'
 
 export const WorkspaceFileUploadPage = () => {
     const { queue } = useFileUploadingStorage()
-    
+
+    useUploadingPageLeaveInterceptor()
+	
     return (
-        <div>
-            <UploadFilesDropzone />
-            { queue.map((data) => (
-                <FileUploadingCard
-                    data={ data }
-                    key={ data.id }
-                />   
-            )) }
-        </div>
+        <LayoutBodyContainer>
+            <ScrollArea verticalMargin={ 24 }>
+                <div className='max-w-screen-md w-full mx-auto p-7'>
+                    <UploadFilesDropzone />
+                    <FileUploader />
+                    { queue.map((data) => (
+                        <FileUploadingCard
+                            data={ data }
+                            key={ data.id }
+                        />   
+                    )) }
+                </div>
+            </ScrollArea>
+        </LayoutBodyContainer>
     )
 }
--- client/src/modules/workspace/providers/WorkspaceProvider.tsx
+++ client/src/modules/workspace/providers/WorkspaceProvider.tsx
@@ -5,20 +5,16 @@ import { workspaceQuery } from '../api/queries/workspaceQuery'
 import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { WorkspaceErrorHandler } from '../components/WorkspaceErrorHandler'
-import { FileUploader } from '@modules/file/components/FileUploader'
 import { workspaceEntityCountsQuery } from '../api/queries/workspaceEntityCountsQuery'
 import { WorkspaceLoadingStatePage } from '../pages/WorkspaceLoadingStatePage'
 
 const WorkspaceProviderContent = () => {
     const params = useParams<{ workspaceSlug: string }>()
-
     const { data: workspace } = useSuspenseQuery(workspaceQuery(params.workspaceSlug!))
-
     const { data: entityCounts } = useSuspenseQuery(workspaceEntityCountsQuery(params.workspaceSlug!))
 
     return (
         <WorkspaceContext.Provider value={ { ...workspace, _count: entityCounts } }>
-            <FileUploader />
             <Outlet />
         </WorkspaceContext.Provider>
     )
