96bbd87649e75be4c589261f2b0aabc260f587d3
514sid <140138716+514sid@users.noreply.github.com>
2025-06-11 18:52:34 +0400

    refactor: update index.ts to include all member controller exports

--- server/src/modules/member/controllers/index.ts
+++ server/src/modules/member/controllers/index.ts
@@ -1,5 +1,18 @@
 import { getWorkspaceMembers } from './getWorkspaceMembers.js'
+import { leaveWorkspace } from './leaveWorkspace.js'
+import { removeMember } from './removeMember.js'
+import { updateMember } from './updateMember.js'
+
+export {
+    getWorkspaceMembers,
+    leaveWorkspace,
+    removeMember,
+    updateMember
+}
 
 export default {
-    getWorkspaceMembers
+    getWorkspaceMembers,
+    leaveWorkspace,
+    removeMember,
+    updateMember
 }
