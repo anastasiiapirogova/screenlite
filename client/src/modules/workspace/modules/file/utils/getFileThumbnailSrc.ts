ed47cb9ee214a96bd604acad0b3a6f83627e5896
514sid <140138716+514sid@users.noreply.github.com>
2025-06-13 01:47:13 +0400

    fix: getFileThumbnailSrc to use URL constructor for thumbnail path

--- client/src/modules/workspace/modules/file/utils/getFileThumbnailSrc.ts
+++ client/src/modules/workspace/modules/file/utils/getFileThumbnailSrc.ts
@@ -1,5 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL
-
 export const getFileThumbnailSrc = (path: string) => {
-    return `${API_URL}/static/thumbnail/${path}`
+    return new URL(`/api/static/thumbnail/${path}`, import.meta.env.VITE_API_URL).toString()
 }
