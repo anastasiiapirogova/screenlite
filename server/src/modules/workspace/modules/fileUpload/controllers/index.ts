5472427ee93924f7f15a021ada6d3f4ca8f3bac4
514sid <140138716+514sid@users.noreply.github.com>
2025-06-21 00:17:04 +0400

    feat: add uploadFilePart export to file upload controllers for handling file part uploads

--- server/src/modules/workspace/modules/fileUpload/controllers/index.ts
+++ server/src/modules/workspace/modules/fileUpload/controllers/index.ts
@@ -1,12 +1,15 @@
 import { createFileUploadSession } from './createFileUploadSession.js'
 import { cancelFileUploading } from './cancelFileUploading.js'
+import { uploadFilePart } from './uploadFilePart.js'
 
 export {
     createFileUploadSession,
     cancelFileUploading,
+    uploadFilePart
 }
 
 export default {
     createFileUploadSession,
     cancelFileUploading,
+    uploadFilePart,
 }
