c5f8a8f421d01784cf5aa2865246b8ea609dc9dc
514sid <140138716+514sid@users.noreply.github.com>
2025-06-21 00:15:01 +0400

    chore: remove file upload part handling and related utilities

--- server/src/modules/workspace/modules/file/utils/addFileUploadCompleteJob.ts
+++
@@ -1,8 +0,0 @@
-import { fileUploadQueue } from 'bullmq/queues/fileUploadQueue.js'
-
-export const addFileUploadCompleteJob = async (fileUploadSessionId: string) => {
-    await fileUploadQueue.add(
-        'mergeFileParts',
-        { fileUploadSessionId },
-    )
-}
