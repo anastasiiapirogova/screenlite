e5cd4d81f037905ce59fdcc3dab810eadfb30537
514sid <140138716+514sid@users.noreply.github.com>
2025-06-21 00:14:27 +0400

    feat: add maximum file size validation to file upload schema and introduce file part upload schema

--- server/src/modules/workspace/modules/fileUpload/schemas/fileUploadSchemas.ts
+++ server/src/modules/workspace/modules/fileUpload/schemas/fileUploadSchemas.ts
@@ -11,9 +11,15 @@ const validateMimeType = (name: string, providedMime?: string) => {
 
 export const createFileUploadSessionSchema = z.object({
     name: fileNameSchema,
-    size: z.preprocess((val) => Number(val), z.number({
-        invalid_type_error: 'FILE_SIZE_IS_INVALID',
-    }).int('FILE_SIZE_IS_INVALID').positive('FILE_SIZE_IS_INVALID')),
+    size: z.preprocess(
+        (val) => Number(val),
+        z.number({
+            invalid_type_error: 'FILE_SIZE_IS_INVALID',
+        })
+            .int('FILE_SIZE_IS_INVALID')
+            .positive('FILE_SIZE_IS_INVALID')
+            .max(2_147_483_648, 'FILE_SIZE_TOO_LARGE') // 2GB max
+    ),
     mimeType: z.string({ invalid_type_error: 'MIME_TYPE_IS_INVALID' }).optional(),
     folderId: z.string().optional(),
 }).refine(
@@ -30,3 +36,7 @@ export const createFileUploadSessionSchema = z.object({
     ...data,
     mimeType: validateMimeType(data.name, data.mimeType)!
 }))
+
+export const filePartUploadSchema = z.object({
+    'fileUploadSessionId': z.string().nonempty('FILE_UPLOAD_SESSION_ID_IS_REQUIRED'),
+})
