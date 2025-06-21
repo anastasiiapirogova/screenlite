add781344a844803733ca7201a4362d3d1ad6080
514sid <140138716+514sid@users.noreply.github.com>
2025-06-21 21:59:49 +0400

    refactor: update MultipartFileUploaderInterface to use Request object and modify completeUpload return type

--- server/src/modules/workspace/modules/fileUpload/services/multipartFileUploader/MultipartFileUploaderInterface.ts
+++ server/src/modules/workspace/modules/fileUpload/services/multipartFileUploader/MultipartFileUploaderInterface.ts
@@ -1,10 +1,10 @@
 import { FileUploadSession } from '@generated/prisma/client.js'
-import { Readable } from 'stream'
+import { Request } from 'express'
 
 export interface MultipartFileUploaderProviderInterface {
     initializeUpload(fileUploadSession: FileUploadSession): Promise<{ uploadId: string }>
-    uploadPart(fileUploadSession: FileUploadSession, body: Buffer | Readable, partNumber: number): Promise<void>
+    uploadPart(fileUploadSession: FileUploadSession, req: Request, partNumber: number): Promise<void>
     confirmPartUpload(fileUploadSession: FileUploadSession, partNumber: number): Promise<void>
-    completeUpload(fileUploadSession: FileUploadSession): Promise<void>
+    completeUpload(fileUploadSession: FileUploadSession): Promise<boolean>
     abortUpload(fileUploadSession: FileUploadSession): Promise<void>
 } 
