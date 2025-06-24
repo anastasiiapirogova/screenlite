48cbd4ce6e55e60310c659d7676eee8618b8a0aa
514sid <140138716+514sid@users.noreply.github.com>
2025-06-25 00:29:49 +0400

    refactor: remove unused asyncFilter and getEffectivePermissions utility functions

--- server/src/utils/asyncFilter.ts
+++
@@ -1,5 +0,0 @@
-export const asyncFilter = async <T>(arr: T[], predicate: (value: T, index: number, array: T[]) => Promise<boolean>): Promise<T[]> => {
-    const results = await Promise.all(arr.map(predicate))
-
-    return arr.filter((_v, index) => results[index])
-}
