f163a46f6b7a926336fde3aa2933aeb9134c1053
514sid <140138716+514sid@users.noreply.github.com>
2025-06-25 21:53:16 +0400

    feat: enhance FileCard component with selection and dragging state management

--- client/src/modules/workspace/modules/file/components/FileCard.tsx
+++ client/src/modules/workspace/modules/file/components/FileCard.tsx
@@ -3,29 +3,42 @@ import React from 'react'
 import { useDraggable } from '@dnd-kit/core'
 import { FileCardBody } from './FileCardBody'
 import { WorkspaceFile } from '../types'
+import { useSelectionStore } from '@stores/useSelectionStore'
+import { useShallow } from 'zustand/react/shallow'
 
 interface PlaylistSectionItemCardProps extends React.HTMLAttributes<HTMLDivElement> {
 	file: WorkspaceFile
 	isDragging?: boolean
 }
 
-export const FileCard = forwardRef<HTMLDivElement, PlaylistSectionItemCardProps>(({ file, ...props }, ref) => {
+export const FileCard = forwardRef<HTMLDivElement, PlaylistSectionItemCardProps>(({ file, onClick, ...props }, ref) => {
+    const { isSelected, isDragging } = useSelectionStore(useShallow((state) => ({
+        isSelected: state.isSelected,
+        isDragging: state.isDragging,
+    })))
+
+    const selected = isSelected(file.id)
+
     return (
         <div
             { ...props }
+            data-entity="file"
             className={ [
-                'cursor-default p-3',
-                'hover:bg-gray-100',
+                'cursor-default p-3 outline-none',
+                !selected && !isDragging && 'hover:bg-gray-100',
+                selected && 'bg-blue-100',
+                (isDragging && selected) && 'opacity-50'
             ].join(' ') }
             ref={ ref }
+            onClick={ onClick }
         >
             { createElement(FileCardBody, { file }) }
         </div>
     )
 })
 
-export const DraggableFileCard = (props: { file: WorkspaceFile }) => {
-    const { file } = props
+export const DraggableFileCard = (props: { file: WorkspaceFile, onClick?: (e: React.MouseEvent) => void }) => {
+    const { file, onClick } = props
 
     const {
         attributes,
@@ -35,7 +48,7 @@ export const DraggableFileCard = (props: { file: WorkspaceFile }) => {
         id: file.id,
         data: {
             file,
-            action: 'sort',
+            action: 'dragFile',
             modifiers: [],
         }
     })
@@ -50,6 +63,7 @@ export const DraggableFileCard = (props: { file: WorkspaceFile }) => {
             file={ file }
             ref={ setNodeRef }
             style={ style }
+            onClick={ onClick }
             { ...attributes }
             { ...listeners }
         />
