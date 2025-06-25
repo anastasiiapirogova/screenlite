ac08ac69b3d37dd9f2df4dbbc6fede50c602d97e
514sid <140138716+514sid@users.noreply.github.com>
2025-06-25 21:54:34 +0400

    feat: enhance drag-and-drop functionality in FilesDndContext with improved selection handling and state management

--- client/src/modules/workspace/modules/file/components/FilesDndContext.tsx
+++ client/src/modules/workspace/modules/file/components/FilesDndContext.tsx
@@ -1,25 +1,50 @@
-import { DndContext, DragEndEvent, DragOverlay, PointerSensor, pointerWithin, useSensor, useSensors } from '@dnd-kit/core'
-import { useSelectionStore } from '@stores/createItemStore'
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent, PointerSensor, pointerWithin, useSensor, useSensors } from '@dnd-kit/core'
+import { useSelectionStore } from '@stores/useSelectionStore'
 import { useShallow } from 'zustand/react/shallow'
 import { ReactNode } from 'react'
 import { snapLeftTopToCursor } from '../utils/snapLeftTopToCursor'
 import { useDragStore } from '@stores/useDragStore'
 
 export const FilesDndContext = ({ children }: { children: ReactNode }) => {
-    const { selectedItems } = useSelectionStore(useShallow((state) => ({
+    const { selectedItems, setDragging, selectItem, clearSelection, isSelected } = useSelectionStore(useShallow((state) => ({
         selectedItems: state.selectedItems,
+        setDragging: state.setDragging,
+        selectItem: state.selectItem,
+        clearSelection: state.clearSelection,
+        isSelected: state.isSelected,
     })))
 
     const { draggedItem, setDraggedItem } = useDragStore()
 
     const selectedCount = Object.keys(selectedItems).length
 
+    const handleDragStart = (event: DragStartEvent) => {
+        const { active } = event
+
+        if (active.data.current?.file && !isSelected(active.data.current.file.id)) {
+            clearSelection()
+            selectItem({ item: active.data.current.file, entity: 'file' })
+        }
+
+        if (active.data.current?.folder && !isSelected(active.data.current.folder.id)) {
+            clearSelection()
+            selectItem({ item: active.data.current.folder, entity: 'folder' })
+        }
+
+        setDragging(true)
+        setDraggedItem(active)
+    }
+
     const handleDragEnd = (event: DragEndEvent) => {
         const { over } = event
 
+        setDragging(false)
+
         if (!over || selectedItems[over.id]) return
 
-        console.log(over)
+        console.log(event.active.data.current?.action)
+
+        setDraggedItem(null)
     }
 
     const sensors = useSensors(
@@ -32,13 +57,8 @@ export const FilesDndContext = ({ children }: { children: ReactNode }) => {
 
     return (
         <DndContext
-            onDragStart={ ({ active }) => {
-                setDraggedItem(active)
-            } }
-            onDragEnd={ (event) =>  {
-                handleDragEnd(event)
-                setDraggedItem(null)
-            } }
+            onDragStart={ handleDragStart }
+            onDragEnd={ handleDragEnd }
             modifiers={ draggedItem ? draggedItem.data.current?.modifiers : [] }
             collisionDetection={ pointerWithin }
             sensors={ sensors }
