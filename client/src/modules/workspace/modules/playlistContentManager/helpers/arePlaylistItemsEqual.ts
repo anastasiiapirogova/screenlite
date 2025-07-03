61affb090b85ba27bd8f5104d760adaa2b291406
514sid <140138716+514sid@users.noreply.github.com>
2025-07-03 17:06:26 +0400

    refactor: consolidate playlist item comparison and mapping functions into PlaylistContentService for improved organization and reusability

--- client/src/modules/workspace/modules/playlistContentManager/helpers/arePlaylistItemsEqual.ts
+++
@@ -1,8 +0,0 @@
-import { areItemsEqual } from '@shared/helpers/areItemsEqual.js'
-import { PlaylistContentManagerItem } from '../../playlist/types.js'
-
-const playlistComparableFields: (keyof PlaylistContentManagerItem)[] = ['type', 'duration', 'order', 'nestedPlaylistId', 'fileId']
-
-export const arePlaylistItemsEqual = (a: PlaylistContentManagerItem[], b: PlaylistContentManagerItem[]) => {
-    return areItemsEqual(a, b, playlistComparableFields)
-}
