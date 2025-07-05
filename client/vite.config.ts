676f919b7795f8201c78a17aac46c8239828f3ac
514sid <140138716+514sid@users.noreply.github.com>
2025-07-05 16:03:57 +0400

    chore: add VITE_HMR_HOST environment variable to docker-compose.yml and update vite.config.ts to use it for hot module replacement configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         host: '0.0.0.0',
         hmr: {
             protocol: 'ws',
-            host: '0.0.0.0',
+            host: process.env.VITE_HMR_HOST || '0.0.0.0',
             port: parseInt(process.env.VITE_PORT || '3001')
         }
     }
