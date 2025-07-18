7c71827305c4ca8d74daa688ce01ee417d1fff73
514sid <140138716+514sid@users.noreply.github.com>
2025-07-18 19:31:46 +0400

    refactor: extend ConfigServiceInterface to include new configuration types for secrets, FFmpeg, and S3 buckets

--- server/src/infrastructure/config/config.interface.ts
+++ server/src/infrastructure/config/config.interface.ts
@@ -3,13 +3,19 @@ import {
     DatabaseConfig,
     RedisConfig,
     S3Config,
-    StorageConfig
+    S3BucketsConfig,
+    StorageConfig,
+    SecretsConfig,
+    FFmpegConfig
 } from './types/index.ts'
 
 export type ConfigServiceInterface = {
     app: AppConfig
     database: DatabaseConfig
     redis: RedisConfig
-    s3: S3Config
     storage: StorageConfig
+    secrets: SecretsConfig
+    ffmpeg: FFmpegConfig
+    s3Buckets: S3BucketsConfig
+    s3?: S3Config
 }
