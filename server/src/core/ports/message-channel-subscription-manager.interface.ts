0c3d38d0bbfffdbc97e4ed614dbfb15f0666a494
514sid <140138716+514sid@users.noreply.github.com>
2025-07-21 22:26:05 +0400

    refactor: remove IMessageChannelSubscriptionManager interface and its implementation from the messaging service

--- server/src/core/ports/message-channel-subscription-manager.interface.ts
+++
@@ -1,5 +0,0 @@
-export type IMessageChannelSubscriptionManager = {
-    subscribe(channel: string, handler: (message: string) => void): Promise<void>
-    unsubscribe(channel: string): Promise<void>
-    shutdown(): Promise<void>
-}
