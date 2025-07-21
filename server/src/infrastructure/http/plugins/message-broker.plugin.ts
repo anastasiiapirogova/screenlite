7c1befcb1507049a6d0d6a9b84b953626a3d25ba
514sid <140138716+514sid@users.noreply.github.com>
2025-07-21 22:27:25 +0400

    refactor: remove IMessageChannelSubscriptionManager and related logic from message broker plugin

--- server/src/infrastructure/http/plugins/message-broker.plugin.ts
+++ server/src/infrastructure/http/plugins/message-broker.plugin.ts
@@ -2,13 +2,10 @@ import fp from 'fastify-plugin'
 import type { FastifyPluginAsync } from 'fastify'
 import { RedisMessageBrokerAdapter } from '@/infrastructure/messaging/adapters/redis-message-broker.adapter.ts'
 import { IMessageBroker } from '@/core/ports/messaging.interface.ts'
-import { IMessageChannelSubscriptionManager } from '@/core/ports/message-channel-subscription-manager.interface.ts'
-import { MessageChannelSubscriptionManager } from '@/infrastructure/messaging/services/message-channel-subscription-manager.service.ts'
 
 declare module 'fastify' {
     interface FastifyInstance {
         messageBroker: IMessageBroker
-        messageChannelSubscriptionManager: IMessageChannelSubscriptionManager
     }
 }
 
@@ -20,14 +17,8 @@ const messageBrokerPlugin: FastifyPluginAsync = async (fastify) => {
     fastify.redis.registerClient('message-broker-sub', redisSub)
 
     const messageBroker = new RedisMessageBrokerAdapter(redisPub, redisSub)
-    const messageChannelSubscriptionManager = new MessageChannelSubscriptionManager(messageBroker)
 
     fastify.decorate('messageBroker', messageBroker)
-    fastify.decorate('messageChannelSubscriptionManager', messageChannelSubscriptionManager)
-
-    fastify.addHook('preClose', async () => {
-        await messageChannelSubscriptionManager.shutdown()
-    })
 
     fastify.addHook('onClose', async () => {
         await fastify.redis.destroyClient('message-broker-pub')
