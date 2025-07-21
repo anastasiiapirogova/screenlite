3288343db8ceafacbe0f839c449010097f83a7d6
514sid <140138716+514sid@users.noreply.github.com>
2025-07-21 19:44:47 +0400

    feat: add ImageProcessingOptions type and update IImageProcessor interface to include processImage method

--- server/src/core/ports/image-processor.interface.ts
+++ server/src/core/ports/image-processor.interface.ts
@@ -5,6 +5,14 @@ export type ImageMetadata = {
     height: number
 }
 
+export type ImageProcessingOptions = {
+    width?: number
+    height?: number
+    format?: 'jpeg' | 'png' | 'webp'
+    quality?: number
+}
+
 export type IImageProcessor = {
     getImageMetadata(readStream: Readable): Promise<ImageMetadata>
+    processImage(imageBuffer: Buffer, options?: ImageProcessingOptions): Promise<Buffer>
 }
