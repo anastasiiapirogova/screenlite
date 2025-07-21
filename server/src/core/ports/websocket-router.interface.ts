528b27ba093cdaac8ad7fba877c498eaa1e107d4
514sid <140138716+514sid@users.noreply.github.com>
2025-07-21 19:45:02 +0400

    feat: add IWebSocketRouter interface for handling WebSocket connections and messages

---
+++ server/src/core/ports/websocket-router.interface.ts
@@ -0,0 +1,9 @@
+import { WebSocketConnection } from '../entities/websocket-connection.entity.ts'
+
+export type MessageHandler = (connection: WebSocketConnection, message: object) => Promise<void> | void
+
+export type IWebSocketRouter = {
+    registerHandler(messageType: string, handler: MessageHandler): void
+    onConnection(connectionId: string): void
+    onMessage(connectionId: string, message: Buffer): void
+}
