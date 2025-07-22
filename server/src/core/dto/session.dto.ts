5ab49f25960c627f307eae61fa0112986d4baca1
514sid <140138716+514sid@users.noreply.github.com>
2025-07-22 23:44:45 +0400

    feat: add SessionDTO type for session management in the application

---
+++ server/src/core/dto/session.dto.ts
@@ -0,0 +1,11 @@
+export type SessionDTO = {
+    id: string
+    userId: string
+    token: string
+    userAgent: string
+    ipAddress: string
+    location: string | null
+    terminatedAt: Date | null
+    lastActivityAt: Date
+    twoFaVerifiedAt: Date | null
+}
