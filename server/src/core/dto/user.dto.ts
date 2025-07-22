a9223a42b75dfb762b2e7c7c66d016d5d96b6d28
514sid <140138716+514sid@users.noreply.github.com>
2025-07-22 23:44:50 +0400

    feat: create UserDTO type for user data representation in the application

---
+++ server/src/core/dto/user.dto.ts
@@ -0,0 +1,12 @@
+export type UserDTO = {
+    id: string
+    email: string
+    name: string
+    password: string
+    emailVerifiedAt: Date | null
+    passwordUpdatedAt: Date | null
+    profilePhoto: string | null
+    totpSecret: string | null
+    twoFactorEnabled: boolean
+    deletedAt: Date | null
+} 
