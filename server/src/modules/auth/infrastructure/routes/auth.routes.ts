91f4eff1c19349ba027e284f3239d99cc8777fe6
514sid <140138716+514sid@users.noreply.github.com>
2025-07-23 14:50:49 +0400

    feat: add login and me routes to authentication module for user session management

--- server/src/modules/auth/infrastructure/routes/auth.routes.ts
+++ server/src/modules/auth/infrastructure/routes/auth.routes.ts
@@ -1,11 +1,15 @@
 import { FastifyInstance } from 'fastify'
 import { signupRoute } from './signup.route.ts'
+import { loginRoute } from './login.route.ts'
+import { meRoute } from './me.route.ts'
 
 // Prefix: /api/auth
 const authRoutes = async (fastify: FastifyInstance) => {
     await Promise.all([
         signupRoute(fastify),
+        loginRoute(fastify),
+        meRoute(fastify)
     ])
-} 
+}
 
 export default authRoutes
---
+++ server/src/modules/auth/infrastructure/routes/me.route.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+export const meRoute = async (fastify: FastifyInstance) => {
+    fastify.withTypeProvider<ZodTypeProvider>().get('/me', {
+        preHandler: [fastify.requireAuth]
+    }, async (request, reply) => {
+        return reply.status(200).send({
+            user: request.user
+        })
+    })
+} 
