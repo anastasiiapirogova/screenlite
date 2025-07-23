dcd01f27cc6b0259e95bad1c6e244c7919815e68
514sid <140138716+514sid@users.noreply.github.com>
2025-07-23 20:37:02 +0400

    feat: enhance error handling to include AuthorizationError for improved authorization feedback

--- server/src/infrastructure/http/plugins/error-handler.plugin.ts
+++ server/src/infrastructure/http/plugins/error-handler.plugin.ts
@@ -1,4 +1,4 @@
-import { HttpError } from '@/core/errors/http.errors.ts'
+import { AuthorizationError } from '@/core/errors/authorization.error.ts'
 import { ValidationError } from '@/core/errors/validation.error.ts'
 import { Prisma } from '@/generated/prisma/client.ts'
 import { FastifyPluginAsync } from 'fastify'
@@ -19,16 +19,6 @@ const errorHandler: FastifyPluginAsync = async (fastify) => {
                 error: 'Service Unavailable',
                 code: 'PRISMA_ERR_SERVICE_UNAVAILABLE',
                 message: 'Unable to connect to the database.',
-            })
-
-            return
-        }
-
-        if (error instanceof HttpError) {
-            reply.status(error.statusCode).send({
-                statusCode: error.statusCode,
-                error: error.error || error.name,
-                message: error.message
             })
 
             return
@@ -62,6 +52,16 @@ const errorHandler: FastifyPluginAsync = async (fastify) => {
             })
 
             return
+        }
+
+        if (error instanceof AuthorizationError) {
+            reply.code(403).send({
+                statusCode: 403,
+                error: 'Forbidden',
+                code: 'FST_ERR_AUTHORIZATION',
+                message: 'Authorization error',
+                errors: error.details,
+            })
         }
 
         reply.send(error)
