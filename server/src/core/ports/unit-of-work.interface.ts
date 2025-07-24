b97745e4823fbcfc2b3753579005767326f036be
514sid <140138716+514sid@users.noreply.github.com>
2025-07-25 00:44:31 +0400

    refactor: add emailVerificationTokenRepository to IUnitOfWork for improved repository access

--- server/src/core/ports/unit-of-work.interface.ts
+++ server/src/core/ports/unit-of-work.interface.ts
@@ -1,3 +1,4 @@
+import { IEmailVerificationTokenRepository } from './email-verification-token-repository.interface.ts'
 import { ISessionRepository } from './session-repository.interface.ts'
 import { IUserRepository } from './user-repository.interface.ts'
 
@@ -5,5 +6,6 @@ export type IUnitOfWork = {
     execute<T>(fn: (repos: {
         userRepository: IUserRepository
         sessionRepository: ISessionRepository
+        emailVerificationTokenRepository: IEmailVerificationTokenRepository
     }) => Promise<T>): Promise<T>
 }
