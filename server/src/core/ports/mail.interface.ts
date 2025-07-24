2a44e5c5bf81f3f548e304f30695db54c32fa8ab
514sid <140138716+514sid@users.noreply.github.com>
2025-07-25 00:43:46 +0400

    refactor: update sendVerificationEmail method to use SendVerificationEmailDTO for improved type safety

--- server/src/core/ports/mail.interface.ts
+++ server/src/core/ports/mail.interface.ts
@@ -1,13 +1,9 @@
 import { MailConfig } from '@/infrastructure/mail/dto/mail-config.dto.ts'
 import { SendEmailDTO } from '@/core/dto/send-email.dto.ts'
-
-export type VerificationEmailData = {
-    email: string
-    token: string
-}
+import { SendVerificationEmailDTO } from '@/core/dto/send-verification-email.dto.ts'
 
 export type IMailService = {
     sendEmail(options: SendEmailDTO): Promise<boolean>
-    sendVerificationEmail(data: VerificationEmailData): Promise<boolean>
+    sendVerificationEmail(data: SendVerificationEmailDTO): Promise<boolean>
     testMailConfig(config: MailConfig): Promise<boolean>
 }
