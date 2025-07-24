07c27e61d4fa97e36ce3bc3fc8a2ed3ed53fcb8e
514sid <140138716+514sid@users.noreply.github.com>
2025-07-25 00:46:40 +0400

    feat: add email verification routes to the HTTP server for handling email verification requests

--- server/src/infrastructure/http/routes/index.ts
+++ server/src/infrastructure/http/routes/index.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from 'fastify'
 import { healthRoutes } from './health.routes.ts'
 import settingRoutes from '@/modules/setting/infrastructure/routes/setting.routes.ts'
 import authRoutes from '@/modules/auth/infrastructure/routes/auth.routes.ts'
-import userRoutes from '@/modules/user/infrastructure/repositories/routes/user.routes.ts'
+import userRoutes from '@/modules/user/infrastructure/routes/user.routes.ts'
+import emailVerificationRoutes from '@/modules/emailVerification/infrastructure/routes/email-verification.routes.ts'
 
 export async function registerRoutes(app: FastifyInstance) {
     app.register(settingRoutes, { prefix: '/api/settings' })
     app.register(healthRoutes, { prefix: '/api/health' })
     app.register(authRoutes, { prefix: '/api/auth' })
     app.register(userRoutes, { prefix: '/api/users' })
+    app.register(emailVerificationRoutes, { prefix: '/api/email-verification' })
 }
