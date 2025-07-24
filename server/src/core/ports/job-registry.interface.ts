8b7b6bf29b2c458f223e9fe64441a4623c302316
514sid <140138716+514sid@users.noreply.github.com>
2025-07-25 00:42:35 +0400

    feat: extend AppJobRegistry interface to include send_verification_email job with SendVerificationEmailDTO

--- server/src/core/ports/job-registry.interface.ts
+++ server/src/core/ports/job-registry.interface.ts
@@ -1,8 +1,12 @@
 import { JobRegistry } from './job-queue.interface.ts'
 import { SendEmailDTO } from '../dto/send-email.dto.ts'
+import { SendVerificationEmailDTO } from '../dto/send-verification-email.dto.ts'
 
 export interface AppJobRegistry extends JobRegistry {
     send_email: {
         data: SendEmailDTO
+    }
+    send_verification_email: {
+        data: SendVerificationEmailDTO
     }
 }
