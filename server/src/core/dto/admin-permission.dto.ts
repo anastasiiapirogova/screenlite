ee3a88f51b5cb78c0ae943bf79716c3ca3ebdbce
514sid <140138716+514sid@users.noreply.github.com>
2025-07-26 01:53:12 +0400

    refactor: change AdminPermissionDTO name property type to AdminPermissionName for improved type safety

--- server/src/core/dto/admin-permission.dto.ts
+++ server/src/core/dto/admin-permission.dto.ts
@@ -1,5 +1,7 @@
+import { AdminPermissionName } from '../enums/admin-permission-name.enum.ts'
+
 export type AdminPermissionDTO = {
     id: string
-    name: string
+    name: AdminPermissionName
     description?: string | null
 }
