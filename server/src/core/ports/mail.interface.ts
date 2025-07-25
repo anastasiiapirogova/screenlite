f54c49e50920265693ea23268b98a8354838e12c
514sid <140138716+514sid@users.noreply.github.com>
2025-07-26 01:54:37 +0400

    refactor: update IMailService to use VerificationEmailDTO instead of SendVerificationEmailDTO for consistency

--- server/src/core/ports/mail.interface.ts
+++ server/src/core/ports/mail.interface.ts
@@ -1,9 +1,9 @@
 import { MailConfig } from '@/infrastructure/mail/dto/mail-config.dto.ts'
 import { SendEmailDTO } from '@/core/dto/send-email.dto.ts'
-import { SendVerificationEmailDTO } from '@/core/dto/send-verification-email.dto.ts'
+import { VerificationEmailDTO } from '@/core/dto/verification-email.dto.ts'
 
 export type IMailService = {
     sendEmail(options: SendEmailDTO): Promise<boolean>
-    sendVerificationEmail(data: SendVerificationEmailDTO): Promise<boolean>
+    sendVerificationEmail(data: VerificationEmailDTO): Promise<boolean>
     testMailConfig(config: MailConfig): Promise<boolean>
 }
