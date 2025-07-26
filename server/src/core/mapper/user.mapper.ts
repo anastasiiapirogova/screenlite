9f94e7391ea89856feaa3cc0de956c450f5355de
514sid <140138716+514sid@users.noreply.github.com>
2025-07-26 13:51:07 +0400

    feat: add UserMapper and IUserMapper interface for user data transformation

---
+++ server/src/core/mapper/user.mapper.ts
@@ -0,0 +1,40 @@
+import { PublicUserDTO } from '../dto/public-user.dto.ts'
+import { UserDTO } from '../dto/user.dto.ts'
+import { User } from '../entities/user.entity.ts'
+import { IUserMapper } from '../ports/user-mapper.interface.ts'
+
+export class UserMapper implements IUserMapper {
+    toDTO(entity: User): UserDTO {
+        return {
+            id: entity.id,
+            email: entity.email,
+            pendingEmail: entity.pendingEmail,
+            name: entity.name,
+            password: entity.password,
+            emailVerifiedAt: entity.emailVerifiedAt,
+            passwordUpdatedAt: entity.passwordUpdatedAt,
+            profilePhoto: entity.profilePhoto,
+            totpSecret: entity.totpSecret,
+            twoFactorEnabled: entity.twoFactorEnabled,
+            deletionRequestedAt: entity.deletionRequestedAt,
+            deletedAt: entity.deletedAt,
+            role: entity.role,
+        }
+    }
+
+    toPublicDTO(entity: User): PublicUserDTO {
+        return {
+            id: entity.id,
+            email: entity.email,
+            pendingEmail: entity.pendingEmail,
+            name: entity.name,
+            role: entity.role,
+            emailVerifiedAt: entity.emailVerifiedAt,
+            passwordUpdatedAt: entity.passwordUpdatedAt,
+            profilePhoto: entity.profilePhoto,
+            twoFactorEnabled: entity.twoFactorEnabled,
+            deletionRequestedAt: entity.deletionRequestedAt,
+            deletedAt: entity.deletedAt,
+        }
+    }
+}
