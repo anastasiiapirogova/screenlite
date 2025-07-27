3ce72a861a46618ecc6627ed58df3b2534c1958e
514sid <140138716+514sid@users.noreply.github.com>
2025-07-27 19:14:24 +0400

    feat: import FastHasher service in login route for enhanced password hashing functionality

--- server/src/modules/auth/infrastructure/routes/login.route.ts
+++ server/src/modules/auth/infrastructure/routes/login.route.ts
@@ -7,9 +7,9 @@ import { BcryptHasher } from '@/shared/infrastructure/services/bcrypt-hasher.ser
 import { loginSchema } from '../schemas/login.schema.ts'
 import { FastifyRequestAdapter } from '@/infrastructure/http/adapters/fastify-request.adapter.ts'
 import { SessionFactory } from '@/modules/session/domain/services/session.factory.ts'
-import { FastHasher } from '@/shared/infrastructure/services/fast-hasher.service.ts'
 import { TokenGenerator } from '@/shared/infrastructure/services/token-generator.service.ts'
 import { UserMapper } from '@/core/mapper/user.mapper.ts'
+import { FastHasher } from '@/shared/infrastructure/services/fast-hasher.service.ts'
 
 export const loginRoute = async (fastify: FastifyInstance) => {
     fastify.withTypeProvider<ZodTypeProvider>().post('/login', {
