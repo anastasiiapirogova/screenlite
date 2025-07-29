4db37d403c25d0d8c2317c52c6cf3badb47a4e2c
514sid <140138716+514sid@users.noreply.github.com>
2025-07-29 20:46:49 +0400

    feat: update getUserPermissionsRoute to include authentication context and refactor user ID parameter handling

--- server/src/modules/admin-permission/infrastructure/routes/get-user-permissions.route.ts
+++ server/src/modules/admin-permission/infrastructure/routes/get-user-permissions.route.ts
@@ -1,30 +1,32 @@
+import { z } from 'zod'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { z } from 'zod'
 import { GetUserAdminPermissionsUseCase } from '@/modules/admin-permission/application/usecases/get-user-admin-permissions.usecase.ts'
 import { PrismaUserAdminPermissionRepository } from '@/modules/admin-permission/infrastructure/repositories/prisma-user-admin-permission.repository.ts'
+import { PrismaUserRepository } from '@/modules/user/infrastructure/repositories/prisma-user.repository.ts'
 
-const getUserPermissionsSchema = z.object({
-    userId: z.uuid()
-})
-
+// Prefix: /api/admin/permissions
 export const getUserPermissionsRoute = async (fastify: FastifyInstance) => {
-    fastify.withTypeProvider<ZodTypeProvider>().get('/user/:userId', {
+    fastify.withTypeProvider<ZodTypeProvider>().get('/users/:userId', {
         schema: {
-            params: getUserPermissionsSchema
+            params: z.object({
+                userId: z.uuid()
+            })
         },
     }, async (request, reply) => {
         const { userId } = request.params
 
+        const authContext = request.auth!
+
         const getUserPermissions = new GetUserAdminPermissionsUseCase(
-            new PrismaUserAdminPermissionRepository(fastify.prisma)
+            new PrismaUserAdminPermissionRepository(fastify.prisma),
+            new PrismaUserRepository(fastify.prisma),
         )
 
-        const permissions = await getUserPermissions.execute(userId)
+        const permissions = await getUserPermissions.execute(authContext, userId)
 
         return reply.status(200).send({
-            userId,
             permissions
         })
     })
-} 
+}
