b468eb48df70f81997b28cf9ac6888dc5e50e697
514sid <140138716+514sid@users.noreply.github.com>
2025-07-29 23:16:12 +0400

    refactor: update import path for AuthContext to use type definition for improved clarity

--- server/src/modules/auth/infrastructure/strategies/session-auth.strategy.ts
+++ server/src/modules/auth/infrastructure/strategies/session-auth.strategy.ts
@@ -4,7 +4,7 @@ import { IHasher } from '@/core/ports/hasher.interface.ts'
 import { ISessionRepository } from '@/core/ports/session-repository.interface.ts'
 import { IUserRepository } from '@/core/ports/user-repository.interface.ts'
 import { ValidateSessionUseCase } from '@/modules/session/application/usecases/validate-session.usecase.ts'
-import { AuthContext } from '@/core/context/auth-context.abstract.ts'
+import { AuthContext } from '@/core/types/auth-context.type.ts'
 import { UserSessionAuthContext } from '@/core/context/user-session-auth.context.ts'
 
 export type SessionAuthDeps = {
