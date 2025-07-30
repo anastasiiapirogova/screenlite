a0642598676466f38c059a374baf990c43c83f5d
514sid <140138716+514sid@users.noreply.github.com>
2025-07-30 17:49:34 +0400

    refactor: remove unused service imports and clean up database connection handling in test setup

--- server/tests/setup.ts
+++ server/tests/setup.ts
@@ -1,10 +1,7 @@
 import { afterAll, beforeAll } from 'vitest'
-import { services } from 'oldsrc/services/index.ts'
 
 beforeAll(async () => {
-    await services.prisma.connect()
 })
 
 afterAll(() => {
-    services.prisma.disconnect()
 })
