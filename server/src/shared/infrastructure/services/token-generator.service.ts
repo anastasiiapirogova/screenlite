9eb04e200544119bd556cbfb7d787f95fc0cd5ee
514sid <140138716+514sid@users.noreply.github.com>
2025-07-31 19:50:30 +0400

    feat: enhance TokenGenerator to support token prefixing in generated tokens

--- server/src/shared/infrastructure/services/token-generator.service.ts
+++ server/src/shared/infrastructure/services/token-generator.service.ts
@@ -2,7 +2,9 @@ import crypto from 'crypto'
 import { ITokenGenerator } from '@/core/ports/token-generator.interface.ts'
 
 export class TokenGenerator implements ITokenGenerator {
-    generate(length = 32): string {
-        return crypto.randomBytes(length).toString('base64url')
+    generate(length = 32, prefix = ''): string {
+        const token = crypto.randomBytes(length).toString('base64url')
+
+        return `${prefix}${token}`
     }
 }
