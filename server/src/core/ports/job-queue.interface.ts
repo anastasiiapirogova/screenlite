9f80766f839f407149acc91e74874a00e537dc0d
514sid <140138716+514sid@users.noreply.github.com>
2025-07-31 19:49:59 +0400

    refactor: update job queue and registry interfaces to use generic data structure and add password reset email job type

--- server/src/core/ports/job-queue.interface.ts
+++ server/src/core/ports/job-queue.interface.ts
@@ -1,25 +1,21 @@
-export type JobRegistry = {
-    [jobType: string]: {
-        data: unknown
-    }
-}
-  
-export interface IJobWorker<T extends JobRegistry = JobRegistry> {
+export interface IJobWorker<T extends { [K in keyof T]: { data: unknown } }> {
     register<J extends keyof T>(
         jobType: J,
-        handler: (data: T[J]['data']) => Promise<void>
+        handler: (data: T[J]['data']) => Promise<void>,
     ): void
     shutdown(): Promise<void>
 }
-
-export interface IJobProducer<T extends JobRegistry = JobRegistry> {
+  
+export interface IJobProducer<T extends { [K in keyof T]: { data: unknown } }> {
     enqueue<J extends keyof T>(
         jobType: J,
         payload: T[J]['data'],
     ): Promise<string>
     enqueueMany<J extends keyof T>(
         jobType: J,
-        payload: T[J]['data'][]
+        payload: T[J]['data'][],
     ): Promise<string[]>
     shutdown(): Promise<void>
 }
+
+export type JobRegistry = Record<string, { data: unknown }>
--- server/src/core/ports/job-registry.interface.ts
+++ server/src/core/ports/job-registry.interface.ts
@@ -1,12 +1,15 @@
-import { JobRegistry } from './job-queue.interface.ts'
 import { SendEmailDTO } from '../dto/send-email.dto.ts'
 import { VerificationEmailDTO } from '../dto/verification-email.dto.ts'
+import { PasswordResetEmailDTO } from '../dto/password-reset-email.dto.ts'
 
-export interface AppJobRegistry extends JobRegistry {
+export interface AppJobRegistry {
     send_email: {
         data: SendEmailDTO
     }
     send_verification_email: {
         data: VerificationEmailDTO
+    }
+    send_password_reset_email: {
+        data: PasswordResetEmailDTO
     }
 }
