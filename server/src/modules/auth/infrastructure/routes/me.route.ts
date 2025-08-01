a85e832a6604dda569a041b3aeb396e7593f9108
514sid <140138716+514sid@users.noreply.github.com>
2025-08-01 17:42:01 +0400

    refactor: simplify user context handling by removing UserSessionAuthContext type casting in me.route.ts

--- server/src/modules/auth/infrastructure/routes/me.route.ts
+++ server/src/modules/auth/infrastructure/routes/me.route.ts
@@ -1,4 +1,4 @@
-import { UserSessionAuthContext } from '@/core/context/user-session-auth.context.ts'
+
 import { UserMapper } from '@/core/mapper/user.mapper.ts'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
@@ -6,8 +6,8 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 export const meRoute = async (fastify: FastifyInstance) => {
     fastify.withTypeProvider<ZodTypeProvider>().get('/me', {}, async (request, reply) => {
         const userMapper = new UserMapper()
-
-        const user = request.auth?.isUserContext() ? userMapper.toPublicDTO((request.auth as UserSessionAuthContext).user) : null
+        
+        const user = request.auth?.isUserContext() ? userMapper.toPublicDTO(request.auth.user) : null
 
         return reply.status(200).send({
             user
