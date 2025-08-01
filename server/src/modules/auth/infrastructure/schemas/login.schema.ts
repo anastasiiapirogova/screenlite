ebe2cbd060c99807b749ad867cbe10bf4019b9a3
514sid <140138716+514sid@users.noreply.github.com>
2025-08-01 17:41:52 +0400

    feat: limit password length to 256 characters in login schema to prevent DoS attacks

--- server/src/modules/auth/infrastructure/schemas/login.schema.ts
+++ server/src/modules/auth/infrastructure/schemas/login.schema.ts
@@ -2,5 +2,6 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
     email: z.email(),
-    password: z.string().min(1)
+    // strip password to 256 characters to avoid DoS
+    password: z.string().min(1).transform(val => val.slice(0, 256))
 })
