04c340c727bf31b54c421627ad81d5ee64a4715f
514sid <140138716+514sid@users.noreply.github.com>
2025-08-01 17:41:36 +0400

    feat: update password validation schema to enforce minimum and maximum length requirements

--- server/src/shared/schemas/user-password.schema.ts
+++ server/src/shared/schemas/user-password.schema.ts
@@ -3,4 +3,5 @@ import { z } from 'zod'
 export const passwordSchema = z
     .string()
     .nonempty('PASSWORD_IS_REQUIRED')
-    .min(8, 'PASSWORD_TOO_SHORT')
+    .min(8, 'PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS')
+    .max(256, 'PASSWORD_MUST_BE_LESS_THAN_256_CHARACTERS')
