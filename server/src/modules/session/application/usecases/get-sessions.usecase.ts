01ba25636882f85b34644b042bb60d3ee9ac160b
514sid <140138716+514sid@users.noreply.github.com>
2025-08-05 02:02:10 +0400

    feat: add GetSessionsUsecase to handle session retrieval with authorization policies

---
+++ server/src/modules/session/application/usecases/get-sessions.usecase.ts
@@ -0,0 +1,24 @@
+import { AuthContext } from '@/core/types/auth-context.type.ts'
+import { ISessionRepository } from '@/modules/session/domain/ports/session-repository.interface.ts'
+import { SessionsQueryOptionsDTO } from '../../domain/dto/sessions-query-options.dto.ts'
+import { PaginationResponse } from '@/core/types/pagination.types.ts'
+import { Session } from '@/core/entities/session.entity.ts'
+import { SessionListPolicy } from '../../domain/policies/session-list.policy.ts'
+
+export class GetSessionsUsecase {
+    constructor(
+        private readonly sessionRepository: ISessionRepository
+    ) {}
+
+    async execute(authContext: AuthContext, options?: SessionsQueryOptionsDTO): Promise<PaginationResponse<Session>> {
+        const sessionListPolicy = new SessionListPolicy(authContext)
+
+        if(options?.filters?.userId) {
+            sessionListPolicy.enforceCanViewSessionsForUser(options.filters.userId)
+        } else {
+            sessionListPolicy.enforceCanViewAllSessions()
+        }
+
+        return this.sessionRepository.findAll(options)
+    }
+}
