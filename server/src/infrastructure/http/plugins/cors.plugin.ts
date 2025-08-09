313786bc82b78b39597f33861099534b9d675f61
514sid <140138716+514sid@users.noreply.github.com>
2025-08-09 18:11:45 +0400

    feat: enhance CORS plugin to allow localhost and specific origins with error handling

--- server/src/infrastructure/http/plugins/cors.plugin.ts
+++ server/src/infrastructure/http/plugins/cors.plugin.ts
@@ -4,7 +4,23 @@ import cors from '@fastify/cors'
 
 const corsPlugin: FastifyPluginAsync = async (fastify) => {
     fastify.register(cors, {
-        origin: fastify.config.app.allowedCorsOrigins,
+        origin: (origin, cb) => {
+            if(!origin) return cb(null, true)
+                
+            const hostname = new URL(origin).hostname
+
+            if(hostname === 'localhost' || hostname === '127.0.0.1'){
+                cb(null, true)
+                return
+            }
+
+            if(fastify.config.app.allowedCorsOrigins.includes(origin)){
+                cb(null, true)
+                return
+            }
+
+            cb(new Error('Not allowed'), false)
+        },
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         credentials: true
     })
