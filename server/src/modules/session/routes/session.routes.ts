2b59edb41021b2424d2650feb3c72be8760fd2ee
514sid <140138716+514sid@users.noreply.github.com>
2025-08-12 13:27:37 +0400

    feat: add getSession route to session routes for retrieving individual session details

--- server/src/modules/session/routes/session.routes.ts
+++ server/src/modules/session/routes/session.routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { getUserSessionsRoute } from './get-user-sessions.route.ts'
+import { getSessionRoute } from './get-session.route.ts'
 
 // Prefix: /api/sessions
 const sessionRoutes = async (fastify: FastifyInstance) => {
     await Promise.all([
         getUserSessionsRoute(fastify),
+        getSessionRoute(fastify),
     ])
 }
 
