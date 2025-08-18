6d0190fd8830052e45eb7d61d67cb4291e6cf4a4
514sid <140138716+514sid@users.noreply.github.com>
2025-08-18 17:34:49 +0400

    refactor: simplify /me route by directly using request.auth properties and removing UserMapper

--- server/src/modules/auth/infrastructure/routes/me.route.ts
+++ server/src/modules/auth/infrastructure/routes/me.route.ts
@@ -1,23 +1,16 @@
 
-import { UserMapper } from '@/modules/user/infrastructure/mappers/user.mapper.ts'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export const meRoute = async (fastify: FastifyInstance) => {
     fastify.withTypeProvider<ZodTypeProvider>().get('/me', {}, async (request, reply) => {
-        const userMapper = new UserMapper()
-
         if (!request.auth?.isUserContext()) {
             return fastify.httpErrors.unauthorized()
         }
 
-        const user = userMapper.toPublicDTO(request.auth.user)
-
         return reply.status(200).send({
-            user,
-            sessionId: request.auth.session?.id,
-            twoFactorAuthEnabled: request.auth.twoFactorAuthEnabled,
-            hasCompletedTwoFactorAuth: request.auth.hasCompletedTwoFactorAuth,
+            user: request.auth.user,
+            session: request.auth.session,
         })
     })
 }
