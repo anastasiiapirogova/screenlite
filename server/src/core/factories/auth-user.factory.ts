9bd79d5a85c7faf7ba2cdd96f6a78309a8085874
514sid <140138716+514sid@users.noreply.github.com>
2025-08-23 18:06:28 +0400

    refactor: update AuthUserFactory to utilize UserEmailState and UserDeletionState for email and active status management

--- server/src/core/factories/auth-user.factory.ts
+++ server/src/core/factories/auth-user.factory.ts
@@ -5,11 +5,11 @@ export class AuthUserFactory {
     static fromUser(user: User): AuthUser {
         return new AuthUser({
             id: user.id,
-            email: user.email,
+            email: user.email.current,
             name: user.name,
             hasAdminAccess: user.hasAdminAccess,
             isSuperAdmin: user.isSuperAdmin,
-            isActive: user.isActive,
+            isActive: user.deletionState.isActive,
         })
     }
 }
