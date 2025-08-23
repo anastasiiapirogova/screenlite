2b621b2eba7d2df6c08be096795c66fbe261d9e8
514sid <140138716+514sid@users.noreply.github.com>
2025-08-23 22:30:36 +0400

    feat: add use case for disabling TOTP method with validation and repository integration

---
+++ server/src/modules/two-factor-auth/application/usecases/disable-totp-method.usecase.ts
@@ -0,0 +1,47 @@
+import { AuthContext } from '@/core/types/auth-context.type.ts'
+import { VerifyTotpCodeUsecase } from './verify-totp-code.usecase.ts'
+import { IUserRepository } from '@/modules/user/domain/ports/user-repository.interface.ts'
+import { ITwoFactorMethodRepository } from '../../domain/ports/two-factor-method-repository.interface.ts'
+import { ValidationError } from '@/shared/errors/validation.error.ts'
+import { TwoFactorAuthPolicy } from '../../domain/policies/two-factor-auth.policy.ts'
+import { TwoFactorMethodType } from '@/core/enums/two-factor-method-type.enum.ts'
+
+type DisableTotpMethodUsecaseDeps = {
+    userRepo: IUserRepository
+    twoFactorMethodRepo: ITwoFactorMethodRepository
+    verifyTotpCodeUsecase: VerifyTotpCodeUsecase
+}
+
+export class DisableTotpMethodUsecase {
+    constructor(private readonly deps: DisableTotpMethodUsecaseDeps) {}
+
+    async execute(authContext: AuthContext, userId: string) {
+        const { twoFactorMethodRepo } = this.deps
+
+        const user = await this.deps.userRepo.findById(userId)
+
+        if (!user) {
+            throw new ValidationError({
+                userId: ['USER_NOT_FOUND'],
+            })
+        }
+
+        const twoFactorAuthPolicy = new TwoFactorAuthPolicy(user, authContext)
+
+        twoFactorAuthPolicy.enforceCanDisableTotpMethod()
+
+        const method = await twoFactorMethodRepo.findByUserIdAndType(userId, TwoFactorMethodType.TOTP)
+
+        if (!method) {
+            throw new ValidationError({
+                userId: ['TOTP_METHOD_NOT_FOUND'],
+            })
+        }
+
+        await twoFactorMethodRepo.delete(method.id)
+
+        return {
+            userId,
+        }
+    }
+}
