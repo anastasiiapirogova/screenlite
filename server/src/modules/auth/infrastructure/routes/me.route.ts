acc6b1606680266d3f69c3fb3f2763134dc0eb5c
514sid <140138716+514sid@users.noreply.github.com>
2025-08-26 01:09:35 +0400

    feat: update /me route to allow skipping two-factor authentication and support deleted users

--- server/src/modules/auth/infrastructure/routes/me.route.ts
+++ server/src/modules/auth/infrastructure/routes/me.route.ts
@@ -1,9 +1,13 @@
-
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export const meRoute = async (fastify: FastifyInstance) => {
-    fastify.withTypeProvider<ZodTypeProvider>().get('/me', {}, async (request, reply) => {
+    fastify.withTypeProvider<ZodTypeProvider>().get('/me', {
+        config: {
+            allowSkipTwoFactorAuth: true,
+            allowDeletedUser: true,
+        }
+    }, async (request, reply) => {
         if (!request.auth?.isUserContext()) {
             return fastify.httpErrors.unauthorized()
         }
