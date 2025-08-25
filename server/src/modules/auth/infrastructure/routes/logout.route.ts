21b983733f2477c63abd482d065526b082746d23
514sid <140138716+514sid@users.noreply.github.com>
2025-08-26 01:09:09 +0400

    feat: update logout route to allow skipping two-factor authentication and support deleted users

--- server/src/modules/auth/infrastructure/routes/logout.route.ts
+++ server/src/modules/auth/infrastructure/routes/logout.route.ts
@@ -4,7 +4,12 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { LogoutUsecase } from '../../application/usecases/logout.usecase.ts'
 
 export const logoutRoute = async (fastify: FastifyInstance) => {
-    fastify.withTypeProvider<ZodTypeProvider>().post('/logout', {}, async (request, reply) => {
+    fastify.withTypeProvider<ZodTypeProvider>().post('/logout', {
+        config: {
+            allowSkipTwoFactorAuth: true,
+            allowDeletedUser: true,
+        }
+    }, async (request, reply) => {
         const sessionRepo = new PrismaSessionRepository(fastify.prisma)
 
         const logout = new LogoutUsecase({
