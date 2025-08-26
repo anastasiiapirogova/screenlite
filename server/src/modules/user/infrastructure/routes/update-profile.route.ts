98d62ec9b20a5e4b0f0dda1ca8bc4b9ac89d41d6
514sid <140138716+514sid@users.noreply.github.com>
2025-08-26 21:20:31 +0400

    feat: refactor update-profile route to include userId parameter, enhance multipart validation, and integrate UpdateProfile use case

--- server/src/modules/user/infrastructure/routes/update-profile.route.ts
+++ server/src/modules/user/infrastructure/routes/update-profile.route.ts
@@ -1,29 +1,60 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import multipart from '@fastify/multipart'
+import { UpdateProfileSchema } from '../schemas/update-profile.schema.ts'
+import { UpdateProfileUsecase } from '../../application/usecases/update-profile.usecase.ts'
+import { PrismaUserRepository } from '../repositories/prisma-user.repository.ts'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { SharpImageValidator } from '@/shared/infrastructure/services/sharp-image-validator.service.ts'
+import { SharpImageProcessor } from '@/shared/infrastructure/services/sharp-image-processor.service.ts'
+import { UserMapper } from '../mappers/user.mapper.ts'
+import { PrismaUnitOfWork } from '@/infrastructure/database/prisma-unit-of-work.ts'
+import { userNameSchema } from '@/shared/schemas/user.schemas.ts'
 
 // Prefix: /api/user
 export const updateProfileRoute = async (fastify: FastifyInstance) => {
     await fastify.register(multipart)
 
-    fastify.patch('/:userId/profile', {}, async (request, reply) => {
-        const { profilePicture, name } = await fastify.validateMultipart(request, z.object({
-            name: z.string(),
+    fastify.withTypeProvider<ZodTypeProvider>().patch('/:userId/profile', {
+        schema: {
+            params: z.object({
+                userId: z.string()
+            })
+        }
+    }, async (request, reply) => {    
+        const multipartData = await fastify.validateMultipart(request, z.object({
+            name: userNameSchema,
+            removeProfilePhoto: z.stringbool().optional()
         }), {
-            profilePicture: {
+            profilePhoto: {
                 maxFileSize: 1024 * 1024 * 5,
+                optional: true
             }
         })
 
-        if (profilePicture && Buffer.isBuffer(profilePicture)) {
-            reply.header('Content-Type', 'image/jpeg')
-            reply.header('Content-Length', profilePicture.length)
-            return reply.send(profilePicture)
-        }
+        const { profilePhoto, name, removeProfilePhoto } = UpdateProfileSchema.parse(multipartData)
+
+        const updateProfileUsecase = new UpdateProfileUsecase({
+            storage: fastify.storage,
+            userRepository: new PrismaUserRepository(fastify.prisma),
+            imageValidator: new SharpImageValidator(),
+            imageProcessor: new SharpImageProcessor(),
+            unitOfWork: new PrismaUnitOfWork(fastify.prisma),
+            jobProducer: fastify.jobProducer,
+            userMapper: new UserMapper()
+        })
+
+        const result = await updateProfileUsecase.execute({
+            userId: request.params.userId,
+            name,
+            profilePhotoBuffer: profilePhoto,
+            removeProfilePhoto,
+            authContext: request.auth
+        })
 
         return reply.status(200).send({
             message: 'Profile updated successfully',
-            data: { name }
+            data: result
         })
     })
 }
