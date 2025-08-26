9de84454e1615448b1c97e6afce15efa6fae9176
514sid <140138716+514sid@users.noreply.github.com>
2025-08-26 21:18:59 +0400

    feat: add delete_old_profile_photo job to AppJobRegistry interface for handling profile photo deletions

--- server/src/core/ports/job-registry.interface.ts
+++ server/src/core/ports/job-registry.interface.ts
@@ -1,6 +1,7 @@
 import { SendEmailDTO } from '@/shared/dto/send-email.dto.ts'
 import { VerificationEmailDTO } from '@/shared/dto/verification-email.dto.ts'
 import { PasswordResetEmailDTO } from '@/shared/dto/password-reset-email.dto.ts'
+import { DeleteProfilePhotoDTO } from '@/shared/dto/delete-profile-photo.dto.ts'
 
 export interface AppJobRegistry {
     send_email: {
@@ -11,5 +12,8 @@ export interface AppJobRegistry {
     }
     send_password_reset_email: {
         data: PasswordResetEmailDTO
+    }
+    delete_old_profile_photo: {
+        data: DeleteProfilePhotoDTO
     }
 }
