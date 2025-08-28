475ac1e94f59b4355a5472e98e478ef8c3c049b5
514sid <140138716+514sid@users.noreply.github.com>
2025-08-28 16:46:51 +0400

    feat: update login schema to use currentPasswordSchema for password validation

--- server/src/modules/auth/infrastructure/schemas/login.schema.ts
+++ server/src/modules/auth/infrastructure/schemas/login.schema.ts
@@ -1,7 +1,8 @@
+import { currentPasswordSchema } from '@/shared/schemas/user-password.schema.ts'
 import { z } from 'zod'
 
 export const loginSchema = z.object({
     email: z.email(),
     // strip password to 256 characters to avoid DoS
-    password: z.string().min(1).transform(val => val.slice(0, 256))
+    password: currentPasswordSchema
 })
--- server/src/shared/schemas/user-password.schema.ts
+++ server/src/shared/schemas/user-password.schema.ts
@@ -1,7 +1,15 @@
 import { z } from 'zod'
 
+const passwordMinLength = 8
+const passwordMaxLength = 256
+
 export const passwordSchema = z
     .string()
     .nonempty('PASSWORD_IS_REQUIRED')
-    .min(8, 'PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS')
-    .max(256, 'PASSWORD_MUST_BE_LESS_THAN_256_CHARACTERS')
+    .min(passwordMinLength, `PASSWORD_MUST_BE_AT_LEAST_${passwordMinLength}_CHARACTERS`)
+    .max(passwordMaxLength, `PASSWORD_MUST_BE_LESS_THAN_${passwordMaxLength}_CHARACTERS`)
+
+export const currentPasswordSchema = z
+    .string()
+    .nonempty('PASSWORD_IS_REQUIRED')
+    .max(passwordMaxLength, `PASSWORD_MUST_BE_LESS_THAN_${passwordMaxLength}_CHARACTERS`)
