fd7511a088d9734d6be48c1d2677582a8c30f0a8
514sid <140138716+514sid@users.noreply.github.com>
2025-08-29 23:01:45 +0400

    feat: add method to IWorkspaceMemberRepository for retrieving workspace memberships with pagination by user ID

--- server/src/core/ports/workspace-member-repository.interface.ts
+++ server/src/core/ports/workspace-member-repository.interface.ts
@@ -1,8 +1,12 @@
+import { WorkspaceMembershipWithWorkspaceView } from '@/modules/user/presentation/view-models/workspace-member-with-workspace.view.ts'
 import { WorkspaceMember } from '../entities/workspace-member.entity.ts'
+import { UserWorkspacesQueryOptionsDTO } from '@/modules/user/domain/dto/user-workspaces-query-options.dto.ts'
+import { PaginationResponse } from '../types/pagination.types.ts'
 
 export interface IWorkspaceMemberRepository {
     findByWorkspaceAndUser(workspaceId: string, userId: string): Promise<WorkspaceMember | null>
     countByWorkspace(workspaceId: string): Promise<number>
     save(member: WorkspaceMember): Promise<void>
     delete(memberId: string): Promise<void>
+    findWithWorkspaceByUserId(userId: string, queryOptions: UserWorkspacesQueryOptionsDTO): Promise<PaginationResponse<WorkspaceMembershipWithWorkspaceView>>
 }
