9a9d2d47b21a19fa42cac98a184dd1713c85c2c0
514sid <140138716+514sid@users.noreply.github.com>
2025-08-29 23:03:47 +0400

    feat: implement method to find workspace memberships with pagination by user ID in PrismaWorkspaceMemberRepository

--- server/src/modules/workspace-member/domain/infrastructure/repositories/prisma-workspace-member.repository.ts
+++ server/src/modules/workspace-member/domain/infrastructure/repositories/prisma-workspace-member.repository.ts
@@ -1,6 +1,10 @@
 import { IWorkspaceMemberRepository } from '@/core/ports/workspace-member-repository.interface.ts'
 import { Prisma, PrismaClient, WorkspaceMember } from '@/generated/prisma/client.ts'
 import { PrismaWorkspaceMemberMapper } from '../mappers/prisma-workspace-member.mapper.ts'
+import { WorkspaceMembershipWithWorkspaceView } from '@/modules/user/presentation/view-models/workspace-member-with-workspace.view.ts'
+import { UserWorkspacesQueryOptionsDTO } from '@/modules/user/domain/dto/user-workspaces-query-options.dto.ts'
+import { Paginator } from '@/shared/utils/pagination.util.ts'
+import { PaginationResponse } from '@/core/types/pagination.types.ts'
 
 export class PrismaWorkspaceMemberRepository implements IWorkspaceMemberRepository {
     constructor(private readonly prisma: PrismaClient | Prisma.TransactionClient) {}
@@ -40,5 +44,46 @@ export class PrismaWorkspaceMemberRepository implements IWorkspaceMemberReposito
         await this.prisma.workspaceMember.delete({
             where: { id: memberId }
         })
+    }
+
+    async findWithWorkspaceByUserId(userId: string, queryOptions: UserWorkspacesQueryOptionsDTO): Promise<PaginationResponse<WorkspaceMembershipWithWorkspaceView>> {
+        const { pagination } = queryOptions || {}
+
+        const where: Prisma.WorkspaceMemberWhereInput = {}
+
+        where.userId = userId
+
+        const findManyFn = (skip: number, take: number) => {
+            return this.prisma.workspaceMember.findMany({
+                where: {
+                    userId
+                },
+                include: {
+                    workspace: true
+                },
+                skip,
+                take
+            })
+        }
+        
+        const countFn = () => this.prisma.workspaceMember.count({ where })
+
+        const result = await Paginator.paginate(
+            findManyFn,
+            countFn,
+            pagination
+        )
+
+        return {
+            items: result.items.map((member) => ({
+                membershipId: member.id,
+                workspace: {
+                    id: member.workspace.id,
+                    name: member.workspace.name,
+                    slug: member.workspace.slug
+                }
+            })),
+            meta: result.meta,
+        }
     }
 }
