aee2120169373901b60ea99da3827561d68df779
514sid <140138716+514sid@users.noreply.github.com>
2025-09-02 01:17:43 +0400

    feat: add IWorkspaceInvitationRepository interface for managing workspace invitations with methods for saving, finding, and counting invitations

---
+++ server/src/modules/workspace-invitation/domain/ports/workspace-invitation-repository.interface.ts
@@ -0,0 +1,11 @@
+import { WorkspaceInvitation } from '@/core/entities/workspace-invitation.entity.ts'
+import { PaginationResponse } from '@/core/types/pagination.types.ts'
+import { WorkspaceInvitationsQueryOptionsDTO } from '../dto/workspace-invitations-query-options.dto.ts'
+import { WorkspaceInvitationStatus } from '@/core/enums/workspace-invitation-status.enum.ts'
+
+export interface IWorkspaceInvitationRepository {
+    save(invitation: WorkspaceInvitation): Promise<void>
+    findById(id: string): Promise<WorkspaceInvitation | null>
+    findAll(queryOptions: WorkspaceInvitationsQueryOptionsDTO): Promise<PaginationResponse<WorkspaceInvitation>>
+    countByWorkspaceIdEmailAndStatus(workspaceId: string, email: string, status: WorkspaceInvitationStatus[]): Promise<number>
+}
