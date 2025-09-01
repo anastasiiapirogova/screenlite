86886371dbb74976c51c5978374ee7906539aa5f
514sid <140138716+514sid@users.noreply.github.com>
2025-09-02 01:17:48 +0400

    feat: implement WorkspaceInvitationPolicy to manage permissions for inviting, responding to, and viewing workspace invitations

---
+++ server/src/modules/workspace-invitation/domain/policies/workspace-invitation.policy.ts
@@ -0,0 +1,62 @@
+import { AdminPermissionName } from '@/core/enums/admin-permission-name.enum.ts'
+import { AuthContext } from '@/core/types/auth-context.type.ts'
+import { WorkspaceAccess } from '@/modules/workspace/domain/value-objects/workspace-access.vo.ts'
+import { ForbiddenError } from '@/shared/errors/forbidden.error.ts'
+
+export class WorkspaceInvitationPolicy {
+    static canInviteToWorkspace(authContext: AuthContext, workspaceAccess: WorkspaceAccess): boolean {
+        if(authContext.hasAdminPermission(AdminPermissionName.WORKSPACE_INVITATIONS_CREATE)) {
+            return true
+        }
+
+        if(workspaceAccess.hasAccess) {
+            return true
+        }
+
+        return false
+    }
+
+    static enforceInviteToWorkspace(authContext: AuthContext, workspaceAccess: WorkspaceAccess): void {
+        if(!WorkspaceInvitationPolicy.canInviteToWorkspace(authContext, workspaceAccess)) {
+            throw new ForbiddenError({
+                workspaceId: ['INSUFFICIENT_PERMISSIONS_TO_INVITE_TO_WORKSPACE']
+            })
+        }
+    }
+
+    static canRespondToWorkspaceInvitation(authContext: AuthContext, invitationEmail: string): boolean {
+        if(authContext.isUserContext()) {
+            return authContext.user.email === invitationEmail
+        }
+        
+        return false
+    }
+
+    static enforceRespondToWorkspaceInvitation(authContext: AuthContext, invitationEmail: string): void {
+        if(!WorkspaceInvitationPolicy.canRespondToWorkspaceInvitation(authContext, invitationEmail)) {
+            throw new ForbiddenError({
+                invitationEmail: ['INSUFFICIENT_PERMISSIONS_TO_ACCEPT_WORKSPACE_INVITATION']
+            })
+        }
+    }
+
+    static canViewWorkspaceInvitation(authContext: AuthContext, workspaceAccess: WorkspaceAccess): boolean {
+        if(authContext.hasAdminPermission(AdminPermissionName.WORKSPACE_INVITATIONS_VIEW)) {
+            return true
+        }
+
+        if(workspaceAccess.hasAccess) {
+            return true
+        }
+
+        return false
+    }
+
+    static enforceViewWorkspaceInvitation(authContext: AuthContext, workspaceAccess: WorkspaceAccess): void {
+        if(!WorkspaceInvitationPolicy.canViewWorkspaceInvitation(authContext, workspaceAccess)) {
+            throw new ForbiddenError({
+                workspaceId: ['INSUFFICIENT_PERMISSIONS_TO_VIEW_WORKSPACE_INVITATION']
+            })
+        }
+    }
+}   
