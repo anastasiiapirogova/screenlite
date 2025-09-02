faf8134429efe2afc487b34bfd16c96f989020d0
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 01:43:39 +0400

    feat: add new admin permissions for workspace management and session viewing

--- server/src/modules/admin-permission/domain/definitions/admin-permission.definitions.ts
+++ server/src/modules/admin-permission/domain/definitions/admin-permission.definitions.ts
@@ -21,4 +21,40 @@ export const SYSTEM_ADMIN_PERMISSIONS = [
         name: AdminPermissionName.USERS_MANAGE_ADMIN_PERMISSIONS,
         description: 'Manage user admin permissions',
     },
+    {
+        name: AdminPermissionName.SESSIONS_VIEW,
+        description: 'View sessions',
+    },
+    {
+        name: AdminPermissionName.WORKSPACES_CREATE,
+        description: 'Create workspaces',
+    },
+    {
+        name: AdminPermissionName.WORKSPACES_SOFT_DELETE,
+        description: 'Soft delete workspaces',
+    },
+    {
+        name: AdminPermissionName.WORKSPACES_DELETE,
+        description: 'Delete workspaces',
+    },
+    {
+        name: AdminPermissionName.WORKSPACES_VIEW,
+        description: 'View workspaces',
+    },
+    {
+        name: AdminPermissionName.WORKSPACES_UPDATE,
+        description: 'Update workspaces',
+    },
+    {
+        name: AdminPermissionName.WORKSPACE_INVITATIONS_VIEW,
+        description: 'View workspace invitations',
+    },
+    {
+        name: AdminPermissionName.WORKSPACE_INVITATIONS_CREATE,
+        description: 'Create workspace invitations',
+    },
+    {
+        name: AdminPermissionName.WORKSPACE_INVITATIONS_CANCEL,
+        description: 'Cancel workspace invitations',
+    },
 ] as const
