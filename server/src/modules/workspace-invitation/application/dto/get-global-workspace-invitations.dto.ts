4fb25d5e6768a05b8d3a33886c9feeebba41d5bd
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 01:45:51 +0400

    refactor: update GetGlobalWorkspaceInvitationsDTO to use GlobalWorkspaceInvitationsQueryOptionsDTO

--- server/src/modules/workspace-invitation/application/dto/get-global-workspace-invitations.dto.ts
+++ server/src/modules/workspace-invitation/application/dto/get-global-workspace-invitations.dto.ts
@@ -1,7 +1,7 @@
 import { AuthContext } from '@/core/types/auth-context.type.ts'
-import { WorkspaceInvitationsQueryOptionsDTO } from '../../domain/dto/workspace-invitations-query-options.dto.ts'
+import { GlobalWorkspaceInvitationsQueryOptionsDTO } from '../../domain/dto/global-workspace-invitations-query-options.dto.ts'
 
 export type GetGlobalWorkspaceInvitationsDTO = {
     authContext: AuthContext
-    queryOptions: WorkspaceInvitationsQueryOptionsDTO
+    queryOptions: GlobalWorkspaceInvitationsQueryOptionsDTO
 }
