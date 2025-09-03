da9d7f6e09cd8a45d448b367cc5caae72f571c8a
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:47:11 +0400

    refactor: update removeMember method to accept memberId as a string instead of WorkspaceMember object

--- server/src/modules/workspace-member/domain/ports/workspace-member-service.interface.ts
+++ server/src/modules/workspace-member/domain/ports/workspace-member-service.interface.ts
@@ -2,7 +2,7 @@ import { WorkspaceMember } from '@/core/entities/workspace-member.entity.ts'
 
 export interface IWorkspaceMemberService {
     addMember(workspaceId: string, userId: string): Promise<void>
-    removeMember(member: WorkspaceMember): Promise<void>
+    removeMember(memberId: string): Promise<void>
     findMember(workspaceId: string, userId: string): Promise<WorkspaceMember | null>
     isMember(workspaceId: string, userId: string): Promise<boolean>
 }
