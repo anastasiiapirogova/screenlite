17328a50409c66a00cbe3fe1f6e01e81f374181d
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:44:41 +0400

    feat: update getWorkspaceInvitations route to use UUID for workspaceId and refactor use case implementation

--- server/src/modules/workspace/infrastructure/routes/get-workspace-invitations.route.ts
+++ server/src/modules/workspace/infrastructure/routes/get-workspace-invitations.route.ts
@@ -1,16 +1,16 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
-import { GetWorkspaceInvitationsUsecase } from '@/modules/workspace-invitation/application/usecases/get-workspace-invitations.usecase.ts'
 import { paginationSchema } from '@/shared/schemas/pagination.schema.ts'
 import { WorkspaceInvitationStatus } from '@/core/enums/workspace-invitation-status.enum.ts'
+import { GetWorkspaceInvitationsByWorkspaceUsecase } from '@/modules/workspace-invitation/application/usecases/get-workspace-invitations-by-workspace.usecase.ts'
 
 // Prefix: /api/workspaces
 export const getWorkspaceInvitationsRoute = async (fastify: FastifyInstance) => {
     fastify.withTypeProvider<ZodTypeProvider>().get('/:workspaceId/invitations', {
         schema: {
             params: z.object({
-                workspaceId: z.string(),
+                workspaceId: z.uuid(),
             }),
             querystring: paginationSchema.extend({
                 status: z.array(z.enum(WorkspaceInvitationStatus)).optional()
@@ -20,11 +20,12 @@ export const getWorkspaceInvitationsRoute = async (fastify: FastifyInstance) => 
             const { workspaceId } = request.params
             const { page, limit, status } = request.query
 
-            const getWorkspaceInvitationsUseCase = new GetWorkspaceInvitationsUsecase(
+            const getWorkspaceInvitationsUseCase = new GetWorkspaceInvitationsByWorkspaceUsecase(
                 {
                     workspaceRepository: fastify.workspaceRepository,
                     workspaceAccessService: fastify.workspaceAccessService,
-                    workspaceInvitationRepository: fastify.workspaceInvitationRepository
+                    workspaceInvitationRepository: fastify.workspaceInvitationRepository,
+                    workspaceInvariantsService: fastify.workspaceInvariantsService
                 }
             )
 
