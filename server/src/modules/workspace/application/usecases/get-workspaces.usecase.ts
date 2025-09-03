09110fe1a3ed1e626e6fbd016b61c310fb3567d0
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:43:41 +0400

    refactor: update GetWorkspacesUsecase to use IWorkspacesQuery for workspace retrieval

--- server/src/modules/workspace/application/usecases/get-workspaces.usecase.ts
+++ server/src/modules/workspace/application/usecases/get-workspaces.usecase.ts
@@ -1,15 +1,15 @@
-import { IWorkspaceRepository } from '../../domain/ports/workspace-repository.interface.ts'
 import { GetWorkspacesDto } from '../dto/get-workspaces.dto.ts'
 import { WorkspaceListPolicy } from '../../domain/policies/workspace-list.policy.ts'
+import { IWorkspacesQuery } from '../../domain/ports/workspaces-query.interface.ts'
 
 export class GetWorkspacesUsecase {
     constructor(
-        private readonly workspaceRepository: IWorkspaceRepository,
+        private readonly workspacesQuery: IWorkspacesQuery,
     ) {}
 
     async execute(dto: GetWorkspacesDto) {
         WorkspaceListPolicy.enforceViewAllWorkspaces(dto.authContext)
 
-        return this.workspaceRepository.findAll(dto.queryOptions)
+        return this.workspacesQuery.handle(dto.queryOptions)
     }
 }
