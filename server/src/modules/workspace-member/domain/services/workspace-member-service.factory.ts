76073ff08ac3f069331eac634a14616bf5eacae0
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:47:15 +0400

    fix: update import path for IWorkspaceMemberRepository to reflect new module structure

--- server/src/modules/workspace-member/domain/services/workspace-member-service.factory.ts
+++ server/src/modules/workspace-member/domain/services/workspace-member-service.factory.ts
@@ -1,6 +1,6 @@
 import { IUserRepository } from '@/modules/user/domain/ports/user-repository.interface.ts'
 import { WorkspaceMemberService } from './workspace-member.service.ts'
-import { IWorkspaceMemberRepository } from '@/core/ports/workspace-member-repository.interface.ts'
+import { IWorkspaceMemberRepository } from '@/modules/workspace-member/domain/ports/workspace-member-repository.interface.ts'
 import { IWorkspaceRepository } from '@/modules/workspace/domain/ports/workspace-repository.interface.ts'
 
 export type IWorkspaceMemberServiceFactory = (deps: {
