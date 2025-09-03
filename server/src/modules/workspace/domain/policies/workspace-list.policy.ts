c509cd5d6e14c63454357a925963dd9d9f1f4993
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:43:54 +0400

    fix: enhance canViewAllWorkspaces method to return boolean and include detailed error information in ForbiddenError

--- server/src/modules/workspace/domain/policies/workspace-list.policy.ts
+++ server/src/modules/workspace/domain/policies/workspace-list.policy.ts
@@ -4,13 +4,19 @@ import { AuthContext } from '@/core/types/auth-context.type.ts'
 
 export class WorkspaceListPolicy {
     static canViewAllWorkspaces(authContext: AuthContext): boolean {
-        return authContext.hasAdminPermission(AdminPermissionName.WORKSPACES_VIEW)
+        if(authContext.hasAdminPermission(AdminPermissionName.WORKSPACES_VIEW)) {
+            return true
+        }
+
+        return false
     }
 
     static enforceViewAllWorkspaces(authContext: AuthContext): void {
         if(!WorkspaceListPolicy.canViewAllWorkspaces(authContext)) {
             throw new ForbiddenError({
-                userId: ['YOU_CANNOT_VIEW_ALL_WORKSPACES']
+                details: {
+                    userId: ['YOU_CANNOT_VIEW_ALL_WORKSPACES']
+                }
             })
         }
     }
