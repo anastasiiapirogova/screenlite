c917df76ecab31fd5ecf22da896536b5beca13de
514sid <140138716+514sid@users.noreply.github.com>
2025-09-03 23:44:59 +0400

    fix: update workspaceId parameter validation to use UUID format in leaveWorkspace route and refactor use case implementation

--- server/src/modules/workspace/infrastructure/routes/leave-workspace.route.ts
+++ server/src/modules/workspace/infrastructure/routes/leave-workspace.route.ts
@@ -2,35 +2,27 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 import { LeaveWorkspaceUsecase } from '@/modules/workspace-member/application/usecases/leave-workspace.usecase.ts'
-import { ForbiddenError } from '@/shared/errors/forbidden.error.ts'
 
 // Prefix: /api/workspaces
 export const leaveWorkspaceRoute = async (fastify: FastifyInstance) => {
     fastify.withTypeProvider<ZodTypeProvider>().post('/:workspaceId/leave', {
         schema: {
             params: z.object({
-                workspaceId: z.string(),
+                workspaceId: z.uuid(),
             }),
         },
         handler: async (request, reply) => {
             const { workspaceId } = request.params
 
-            if(!request.auth.isUserContext()) {
-                throw new ForbiddenError({
-                    auth: ['ONLY_USER_CAN_LEAVE_WORKSPACE']
-                })
-            }
-
             const leaveWorkspaceUseCase = new LeaveWorkspaceUsecase(
                 {
                     workspaceRepository: fastify.workspaceRepository,
-                    workspaceMemberRepository: fastify.workspaceMemberRepository,
-                    workspaceMemberServiceFactory: fastify.workspaceMemberServiceFactory,
-                    unitOfWork: fastify.unitOfWork
+                    workspaceAccessService: fastify.workspaceAccessService,
+                    workspaceMemberService: fastify.workspaceMemberService,
                 }
             )
 
-            await leaveWorkspaceUseCase.execute(request.auth.user.id, workspaceId)
+            await leaveWorkspaceUseCase.execute(request.auth, workspaceId)
 
             return reply.status(204).send()
         }
